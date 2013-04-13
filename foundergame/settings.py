f077650f764dc35d570c849881c98cc9a8393f20
Mitchell Kates <mkates@mit.edu>
2013-04-13 11:49:13 -0400

    changed settings

--- foundergame/settings.py
+++ foundergame/settings.py
@@ -160,13 +160,13 @@ LOGGING = {
 }
 
 # Parse database configuration from $DATABASE_URL
-#import socket
-#import dj_database_url
-# if socket.gethostname().startswith('Mitchell'):
-#     #Use Local Settings
-#     LOCAL = True
-# else: 
+import socket
+import dj_database_url
+if socket.gethostname().startswith('Mitchell'):
+    #Use Local Settings
+	LOCAL = True
+else: 
 # 	#Use heroku settings
-# 	LOCAL = False
-#     #DATABASES['default'] =  dj_database_url.config()
-#     #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+	LOCAL = False
+	DATABASES['default'] =  dj_database_url.config()
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
