100171f1754672b9e2a590fdb8b6dd2dd5b8b115
Mitchell Kates <mkates@mit.edu>
2013-04-28 15:17:52 -0400

    changes settings to handle heroku

--- foundergame/settings.py
+++ foundergame/settings.py
@@ -166,7 +166,7 @@ if socket.gethostname().startswith('lca'):
     #Use Local Settings
 	LOCAL = True
 else: 
-# 	#Use heroku settings
+ 	#Use heroku settings
 	LOCAL = False
-	#DATABASES['default'] =  dj_database_url.config()
-	#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+	DATABASES['default'] =  dj_database_url.config()
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
